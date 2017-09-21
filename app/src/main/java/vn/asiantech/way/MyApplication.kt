7125bb7c9d8658c4e658e5d4ff126f3d462ad649
at-quocnguyen <quoc.nguyen@asiantech.vn>
2017-09-21 09:41:50 +0700

    setup project

---
+++ app/src/main/java/vn/asiantech/way/MyApplication.kt
@@ -0,0 +1,12 @@
+package vn.asiantech.way
+
+import android.app.Application
+
+/**
+ * Copyright © 2017 Asian Tech Co., Ltd.
+ * Created by quocnguyenp. on 9/21/17.
+ */
+
+class MyApplication : Application() {
+
+}
