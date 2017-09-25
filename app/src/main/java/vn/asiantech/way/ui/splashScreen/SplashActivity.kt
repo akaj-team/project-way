e4f659a7949a2c234f65f6cf6bc0833ad7914aa8
Hang Tran T.D <hang.tran@asiantech.vn>
2017-09-25 17:24:46 +0700

    add permission functions

--- app/src/main/java/vn/asiantech/way/ui/splashScreen/SplashActivity.kt
+++ app/src/main/java/vn/asiantech/way/ui/splashScreen/SplashActivity.kt
@@ -2,35 +2,100 @@ package vn.asiantech.way.ui.splashScreen
 
 import android.animation.ValueAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
 import android.view.animation.LinearInterpolator
+import android.view.animation.ScaleAnimation
+import android.widget.Toast
+import com.hypertrack.lib.HyperTrack
+import com.hypertrack.lib.HyperTrackUtils
 import kotlinx.android.synthetic.main.activity_splash.*
 import vn.asiantech.way.R
 import vn.asiantech.way.ui.base.BaseActivity
-
 /**
 * Created by atHangTran on 22/09/2017.
 */
 
 class SplashActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         setAnimationForBackground()
+        setScaleForCircle()
+        handlePermission()
     }
 
     private fun setAnimationForBackground() {
-        val animator = ValueAnimator.ofFloat(0.0f, 1.0f)
-        animator.repeatCount = ValueAnimator.INFINITE
-        animator.interpolator = LinearInterpolator()
-        animator.duration = 10000L
-        animator.addUpdateListener { animation ->
+        val transAnim = ValueAnimator.ofFloat(0.0f, 1.0f)
+        transAnim.repeatCount = ValueAnimator.INFINITE
+        transAnim.interpolator = LinearInterpolator()
+        transAnim.duration = 10000L
+        transAnim.addUpdateListener { animation ->
             val progress = animation.animatedValue as Float
-            val width = imgBackground1.getWidth()
+            val width = imgBackground1.width
             val translationX = width * progress
             imgBackground1.translationX = translationX
             imgBackground2.translationX = translationX - width
-            animator.start()
-            setAnimationForBackground()
+        }
+        transAnim.start()
+    }
+
+    private fun setScaleForCircle() {
+        val growAnim = ScaleAnimation(0.9f, 1.05f, 0.9f, 1.05f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f)
+        val shrinkAnim = ScaleAnimation(1.05f, 0.9f, 1.05f, 0.9f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f)
+        growAnim.duration = 800
+        shrinkAnim.duration = 800
+        imgCircle.animation = growAnim
+        growAnim.start()
+        growAnim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {}
+
+            override fun onAnimationRepeat(animation: Animation) {}
+
+            override fun onAnimationEnd(animation: Animation) {
+                imgCircle.animation = shrinkAnim
+                shrinkAnim.start()
+            }
+        })
+
+        shrinkAnim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {}
+
+            override fun onAnimationRepeat(animation: Animation) {}
+
+            override fun onAnimationEnd(animation: Animation) {
+                imgCircle.animation = growAnim
+                growAnim.start()
+            }
+        })
+    }
+
+    private fun handlePermission() {
+        btnEnableLocation.setOnClickListener {
+            if (HyperTrackUtils.isInternetConnected(this)) {
+                if (HyperTrackUtils.isLocationEnabled(this)) {
+                    progressBar.visibility = View.VISIBLE
+                    btnEnableLocation.visibility = View.GONE
+                    tvAppDescription.visibility = View.GONE
+                    //TODO: intent to login screen
+
+                } else {
+                    if (!HyperTrack.checkLocationPermission(this)) {
+                        HyperTrack.requestPermissions(this)
+                    }
+
+                    if (!HyperTrack.checkLocationServices(this)) {
+                        HyperTrack.requestLocationServices(this)
+                    }
+                }
+            } else {
+                Toast.makeText(this, R.string.splash_toast_turn_on_wifi, Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
