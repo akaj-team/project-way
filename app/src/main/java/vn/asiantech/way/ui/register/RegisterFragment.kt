d9f9ba7e332fba4075f829c33db6706d0f76519c
at-haingo <hai.ngo@asiantech.vn>
2017-09-27 22:31:13 +0700

    set default and save profile

--- app/src/main/java/vn/asiantech/way/ui/register/RegisterFragment.kt
+++ app/src/main/java/vn/asiantech/way/ui/register/RegisterFragment.kt
@@ -4,20 +4,32 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
-import android.view.KeyEvent
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Base64
+import android.util.Log
+import android.view.*
 import android.view.inputmethod.EditorInfo
 import android.widget.AdapterView
 import android.widget.TextView
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.hypertrack.lib.HyperTrack
+import com.hypertrack.lib.callbacks.HyperTrackCallback
+import com.hypertrack.lib.models.ErrorResponse
+import com.hypertrack.lib.models.SuccessResponse
+import com.hypertrack.lib.models.User
+import com.hypertrack.lib.models.UserParams
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
 import kotlinx.android.synthetic.main.fragment_register.*
 import kotlinx.android.synthetic.main.fragment_register.view.*
 import vn.asiantech.way.R
@@ -30,19 +42,27 @@ import java.io.ByteArrayOutputStream
  * Fragment register
  * Created by haibt on 9/26/17.
  */
-class RegisterFragment : BaseFragment(), TextView.OnEditorActionListener {
+class RegisterFragment : BaseFragment(), TextView.OnEditorActionListener
+        , View.OnClickListener, TextWatcher {
     companion object {
         const private val REQUEST_CODE_PICK_IMAGE = 1001
         const private val REQUEST_CODE_GALLERY = 500
     }
 
+    var mIsoCode: String? = null
+    var mBitmap: Bitmap? = null
+    var mCountries: List<Country> = ArrayList()
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View = inflater!!.inflate(R.layout.fragment_register, container, false)
         initView(view)
+        mCountries = getCountries(readJsonFromDirectory())
         initCountrySpinner(view)
+        setUserInformation(view)
         view.frAvatar.setOnClickListener {
             checkPermissionGallery()
         }
+        mIsoCode = resources.getString(R.string.iso_code_default)
         return view
     }
 
@@ -64,6 +84,103 @@ class RegisterFragment : BaseFragment(), TextView.OnEditorActionListener {
         return false
     }
 
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.btnSave -> {
+                val name: String = edtName.text.toString()
+                val phoneNumber: String = edtPhoneNumber.text.toString()
+                if (name.isBlank()) {
+                    edtName.setText(R.string.user_name_default)
+                }
+                if (phoneNumber.isBlank()) {
+                    edtPhoneNumber.text = null
+                }
+                createUser(name, phoneNumber)
+            }
+            R.id.tvCancel -> {
+                //TODO
+            }
+
+        }
+    }
+
+    override fun afterTextChanged(p0: Editable?) {
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        if (edtName.text.isBlank() && edtPhoneNumber.text.isBlank()) {
+            tvCancel.text = resources.getString(R.string.skip)
+            btnSave.isEnabled = false
+        } else {
+            tvCancel.text = resources.getString(R.string.cancel)
+            btnSave.isEnabled = true
+        }
+    }
+
+    private fun createUser(name: String, phoneNumber: String) {
+        val userParam: UserParams = UserParams()
+                .setName(name)
+                .setPhoto(encodeImage(mBitmap))
+                .setPhone(phoneNumber)
+                .setLookupId(mIsoCode)
+        Log.d("xxx", "" + mBitmap.toString())
+        HyperTrack.getOrCreateUser(userParam, object : HyperTrackCallback() {
+            override fun onSuccess(p0: SuccessResponse) {
+                HyperTrack.startTracking()
+                Log.d("xxx", "success")
+            }
+
+            override fun onError(p0: ErrorResponse) {
+                Log.e("xxx", " create error " + p0.errorMessage)
+            }
+        })
+    }
+
+    private fun setUserInformation(view: View) {
+        visibleProgressBar(view)
+        HyperTrack.getUser(object : HyperTrackCallback() {
+            override fun onSuccess(response: SuccessResponse) {
+                val user: User? = response.responseObject as User
+                updateView(view, user)
+                invisibleProgressBar(view)
+            }
+
+            override fun onError(p0: ErrorResponse) {
+                invisibleProgressBar(view)
+            }
+        })
+    }
+
+    private fun updateView(view: View, user: User?) {
+        val target: Target = object : Target {
+            override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+            }
+
+            override fun onBitmapFailed(errorDrawable: Drawable?) {
+            }
+
+            override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom?) {
+                view.imgAvatar.setImageBitmap(bitmap)
+                mBitmap = bitmap
+            }
+        }
+        Picasso.with(context).load(user?.photo).into(target)
+        view.imgAvatar.tag = target
+        view.edtName.setText(user?.name)
+        view.edtPhoneNumber.setText(user?.phone)
+        val isoCode: String = user!!.lookupId
+        for (i in 0..mCountries.size - 1) {
+            if (isoCode == mCountries[i].iso) {
+                view.spinnerNation.setSelection(i)
+                view.tvBaseNumber.text = mCountries[i].tel
+                break
+            }
+        }
+    }
+
     private fun intentGallery() {
         val intent: Intent = Intent()
         intent.type = "image/*"
@@ -81,21 +198,34 @@ class RegisterFragment : BaseFragment(), TextView.OnEditorActionListener {
         }
     }
 
+    private fun visibleProgressBar(view: View) {
+        view.progressBar.visibility = View.VISIBLE
+        activity.window.setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+    }
+
+    private fun invisibleProgressBar(view: View) {
+        view.progressBar.visibility = View.GONE
+        activity.window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+    }
+
     private fun initView(view: View) {
         view.edtName.setOnEditorActionListener(this)
         view.edtPhoneNumber.setOnEditorActionListener(this)
+        view.btnSave.setOnClickListener(this)
+        view.edtName.addTextChangedListener(this)
+        view.edtPhoneNumber.addTextChangedListener(this)
     }
 
     private fun initCountrySpinner(view: View) {
-        val countries: List<Country> = getCountries(readJsonFromDirectory())
-        view.spinnerNation.adapter = CountrySpinnerAdapter(context, R.layout.item_list_country, countries)
+        view.spinnerNation.adapter = CountrySpinnerAdapter(context, R.layout.item_list_country, mCountries)
         view.spinnerNation.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
-                view.tvBaseNumber.text = resources.getString(R.string.plus).plus(countries[position].tel)
+                mIsoCode = mCountries[position].iso
+                view.tvBaseNumber.text = resources.getString(R.string.plus).plus(mCountries[position].tel)
             }
         }
     }
@@ -119,11 +249,20 @@ class RegisterFragment : BaseFragment(), TextView.OnEditorActionListener {
         return countries
     }
 
+    private fun encodeImage(bitmap: Bitmap?): String {
+        val baoStream = ByteArrayOutputStream()
+        bitmap?.compress(Bitmap.CompressFormat.JPEG, 60, baoStream)
+        val b = baoStream.toByteArray()
+        val encodeImage = Base64.encodeToString(b, Base64.DEFAULT)
+        return encodeImage
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_PICK_IMAGE) {
             val uri: Uri = data!!.data
             imgAvatar.setImageURI(uri)
+            mBitmap = MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
         }
     }
 
