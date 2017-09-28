1ca6bcee4db8505a8f377d6d9d62fe89ef5d9907
at-quocnguyen <quoc.nguyen@asiantech.vn>
2017-09-28 09:44:40 +0700

    add more

--- app/src/main/java/vn/asiantech/way/di/component/AppComponent.kt
+++ app/src/main/java/vn/asiantech/way/di/component/AppComponent.kt
@@ -4,24 +4,22 @@ import android.app.Application
 import dagger.BindsInstance
 import dagger.Component
 import vn.asiantech.way.MyApplication
-import javax.inject.Singleton
-
+import vn.asiantech.way.di.builder.ActivityBuilder
+import vn.asiantech.way.di.module.AppModule
+import vn.asiantech.way.di.scopes.PerApplication
 
 /**
  * Copyright © 2017 Asian Tech Co., Ltd.
  * Created by quocnguyenp. on 9/26/17.
  */
 
-@Singleton
-@Component()
+@PerApplication
+@Component(modules = arrayOf(AppModule::class, ActivityBuilder::class))
 interface AppComponent {
 
     @Component.Builder interface Builder {
-
         @BindsInstance fun application(application: Application): Builder
-
         fun build(): AppComponent
-
     }
 
     fun inject(app: MyApplication)
