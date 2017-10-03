87af0f622eaaf31d05d7f1b495ea2ed8adf2ae51
at-haingo <hai.ngo@asiantech.vn>
2017-10-03 09:44:28 +0700

    merge master

---
+++ scripts/saddler.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [ -z "${CI_PULL_REQUEST}" ]; then
+    # when not pull request
+    REPORTER=Saddler::Reporter::Github::CommitReviewComment
+else
+    REPORTER=Saddler::Reporter::Github::PullRequestReviewComment
+fi
+
+echo "********************"
+echo "* detekt           *"
+echo "********************"
+cat app/build/reports/detekt/detekt-checkstyle.xml \
+    | bundle exec checkstyle_filter-git diff origin/master \
+    | bundle exec saddler report --require saddler/reporter/github --reporter $REPORTER
+
+
+echo "********************"
+echo "* android lint     *"
+echo "********************"
+cat app/build/reports/lint-results.xml \
+    | bundle exec android_lint_translate_checkstyle_format translate \
+    | bundle exec checkstyle_filter-git diff origin/master \
+    | bundle exec saddler report --require saddler/reporter/github --reporter $REPORTER
