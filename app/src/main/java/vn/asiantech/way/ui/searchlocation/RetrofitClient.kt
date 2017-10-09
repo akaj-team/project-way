40fa1365a81865433670f6087d46732e4c6d2d1b
Cuong Cao V <cuong.cao@asiantech.vn>
2017-10-09 10:46:24 +0700

    fix ci comment

--- app/src/main/java/vn/asiantech/way/ui/searchlocation/RetrofitClient.kt
+++ app/src/main/java/vn/asiantech/way/ui/searchlocation/RetrofitClient.kt
@@ -11,17 +11,16 @@ import retrofit2.converter.gson.GsonConverterFactory
 class RetrofitClient {
 
     companion object {
-        private var retrofit: Retrofit? = null
+        private val retrofit: Retrofit by lazy {
+            Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+        }
         private const val BASE_URL = "https://maps.googleapis.com/maps/api/place/textsearch/"
 
         private fun getClient(): Retrofit {
-            if (retrofit == null) {
-                retrofit = Retrofit.Builder()
-                        .baseUrl(BASE_URL)
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build()
-            }
-            return retrofit!!
+            return retrofit
         }
 
         /**
