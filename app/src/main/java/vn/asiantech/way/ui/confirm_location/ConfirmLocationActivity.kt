89ddc3f2944aa6336199c2d2f76b5284ea60c448
at-haingo <hai.ngo@asiantech.vn>
2017-10-12 09:41:42 +0700

    update code

---
+++ app/src/main/java/vn/asiantech/way/ui/confirm_location/ConfirmLocationActivity.kt
@@ -0,0 +1,15 @@
+package vn.asiantech.way.ui.confirm_location
+
+import android.os.Bundle
+import vn.asiantech.way.R
+import vn.asiantech.way.ui.base.BaseActivity
+
+/**
+ * Created by haingoq on 10/10/2017.
+ */
+class ConfirmLocationActivity: BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirm_location)
+    }
+}
