bdae2f1f488089c0f67a89c8038e591e230d3259
at-haingo <hai.ngo@asiantech.vn>
2017-10-13 08:49:46 +0700

    update document

--- app/src/main/java/vn/asiantech/way/ui/views/MenuLinearLayout.kt
+++ app/src/main/java/vn/asiantech/way/ui/views/MenuLinearLayout.kt
@@ -90,13 +90,19 @@ class MenuLinearLayout(context: Context, attrs: AttributeSet) :
      * Interface menu click listener
      */
     internal interface OnMenuClickListener {
-        // Event when button share clicked
+        /**
+         * Event when button share clicked
+         */
         fun onShareClick()
 
-        // Event when button profile clicked
+        /**
+         * Event when button profile clicked
+         */
         fun onProfileClick()
 
-        // Event when button calendar clicked
+        /**
+         * Event when button calendar clicked
+         */
         fun onCalendarClick()
     }
 }
