9171fc32e76900cb0be8b3567eb19728580d49ec
at-haingo <hai.ngo@asiantech.vn>
2017-10-19 15:40:51 +0700

    merge master

--- app/src/main/java/vn/asiantech/way/extension/Activity.kt
+++ app/src/main/java/vn/asiantech/way/extension/Activity.kt
@@ -8,20 +8,22 @@ import android.view.ViewGroup
 import android.widget.Toast
 
 /**
- * Copyright © 2017 Asian Tech Co., Ltd.
- * Created by atHangTran on 26/09/2017.
+ * Copyright © 2017 AsianTech inc.
+ * Created by at-hoavo on 27/09/2017.
  */
 
 /**
- * Extension method to show toast in activities.
+ * Toast message
+ * @param message: CharSequence to show on toast
+ * @param duration: Int, default is Toast.LENGTH_SHORT
  */
-fun Activity.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
+internal fun Activity.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, message, duration).show()
 }
 
 /**
  * Extension method to inflate layout for ViewGroup.
  */
-fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
+internal fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
     return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
 }
