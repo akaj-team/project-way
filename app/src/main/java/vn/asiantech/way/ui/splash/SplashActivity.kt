df96f01b30e5fb23ea5bb7ed8869c5ca3a98686e
hangtrantd <hang.tran@asiantech.vn>
2017-10-26 09:37:58 +0700

    show register screen when wifi and gps turn on

--- app/src/main/java/vn/asiantech/way/ui/splash/SplashActivity.kt
+++ app/src/main/java/vn/asiantech/way/ui/splash/SplashActivity.kt
@@ -1,7 +1,12 @@
 package vn.asiantech.way.ui.splash
 
 import android.animation.ValueAnimator
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.location.LocationManager
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.view.View
 import android.view.animation.Animation
@@ -21,12 +26,53 @@ import vn.asiantech.way.ui.register.RegisterActivity
  */
 class SplashActivity : BaseActivity() {
 
+    private val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if ((intent!!.action == WifiManager.WIFI_STATE_CHANGED_ACTION ||
+                    intent.action == WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
+                val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    toast(getString(R.string.splash_toast_turn_on_gps))
+                } else {
+                    startSwitchScreen()
+                }
+            }
+
+            if (intent.action == LocationManager.PROVIDERS_CHANGED_ACTION) {
+                if (!HyperTrackUtils.isWifiEnabled(context)) {
+                    toast(getString(R.string.splash_toast_turn_on_wifi))
+                } else {
+                    startSwitchScreen()
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION)
+        intentFilter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION)
+        intentFilter.addAction(LocationManager.PROVIDERS_CHANGED_ACTION)
+        registerReceiver(broadcastReceiver, intentFilter)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         setAnimationForBackground()
         setScaleForCircle()
         requestPermission()
+    }
+
+    private fun startSwitchScreen() {
+        progressBar.visibility = View.VISIBLE
+        btnEnableLocation.visibility = View.GONE
+        tvAppDescription.visibility = View.GONE
+        val intent = Intent(this, RegisterActivity::class.java)
+        intent.putExtra(RegisterActivity.INTENT_REGISTER, RegisterActivity.INTENT_CODE_SPLASH)
+        startActivity(intent)
+        finish()
     }
 
     private fun setAnimationForBackground() {
@@ -82,16 +128,14 @@ class SplashActivity : BaseActivity() {
     }
 
     private fun requestPermission() {
+        if (HyperTrackUtils.isInternetConnected(this) && HyperTrackUtils.isLocationEnabled(this)) {
+            startSwitchScreen()
+        }
         btnEnableLocation.setOnClickListener {
-            if (HyperTrackUtils.isInternetConnected(this)) {
-                if (HyperTrackUtils.isLocationEnabled(this)) {
-                    progressBar.visibility = View.VISIBLE
-                    btnEnableLocation.visibility = View.GONE
-                    tvAppDescription.visibility = View.GONE
-                    val intent = Intent(this, RegisterActivity::class.java)
-                    intent.putExtra(RegisterActivity.INTENT_REGISTER, RegisterActivity.INTENT_CODE_SPLASH)
-                    startActivity(intent)
-                } else {
+            if (!HyperTrackUtils.isWifiEnabled(this)) {
+                toast(getString(R.string.splash_toast_turn_on_wifi))
+            } else {
+                if (!HyperTrackUtils.isLocationEnabled(this)) {
                     if (!HyperTrack.checkLocationPermission(this)) {
                         HyperTrack.requestPermissions(this)
                     }
@@ -100,8 +144,6 @@ class SplashActivity : BaseActivity() {
                         HyperTrack.requestLocationServices(this)
                     }
                 }
-            } else {
-                toast(getString(R.string.splash_toast_turn_on_wifi))
             }
         }
     }
