014ff1d0c40ad5411423217675e594c8bfaf29d5
at-toannguyen1 <toannt2@asiantech.vn>
2017-11-17 11:14:10 +0700

    update

--- app/src/main/java/vn/asiantech/way/ui/splash/SplashActivity.kt
+++ app/src/main/java/vn/asiantech/way/ui/splash/SplashActivity.kt
@@ -9,7 +9,6 @@ import android.location.LocationManager
 import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.LinearInterpolator
