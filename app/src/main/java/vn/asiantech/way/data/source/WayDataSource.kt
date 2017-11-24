83711916edba87c493293fa27009c7ebcfd64075
at-hoavo <hoa.vo@asiantech.vn>
2017-11-24 16:37:14 +0700

    fill datasource

--- app/src/main/java/vn/asiantech/way/data/source/WayDataSource.kt
+++ app/src/main/java/vn/asiantech/way/data/source/WayDataSource.kt
@@ -1,12 +1,38 @@
 package vn.asiantech.way.data.source
 
+import com.hypertrack.lib.models.User
+import com.hypertrack.lib.models.UserParams
+import io.reactivex.Observable
+import vn.asiantech.way.data.model.*
+import vn.asiantech.way.data.source.remote.response.LocationResponse
+
 /**
  * Copyright © 2017 AsianTech inc.
  * Created by vinh.huynh on 11/24/17.
  */
 interface WayDataSource {
-    /**
-     * Function get data
-     */
-    fun getWayData()
+
+    fun createUser(userParams:UserParams)
+
+    fun updateUser(userParams:UserParams)
+
+    fun getAddressLocation(latLng: String): Observable<LocationResponse>
+
+    fun getLocationDetail(placeId: String?, key: String): Observable<ResultPlaceDetail>
+
+    fun searchLocations(input: String,
+                        key: String,
+                        language: String = "vi",
+                        sensor: Boolean = false): Observable<AutoCompleteResult>
+
+    fun createGroup(name: String): Observable<Group>
+
+    fun getGroupInfo(groupId: String): Observable<Group>
+
+    fun getMembersList(groupId: String): Observable<UserListResult>
+
+    fun addUserToGroup(userId: String, body: BodyAddUserToGroup): Observable<User>
+
+    fun removeUserFromGroup(userId: String, body: BodyAddUserToGroup): Observable<User>
+
 }
