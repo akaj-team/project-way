127565aa68ef976de3e589af0c69ab8066b377ea
Cuong Cao V <cuong.cao@asiantech.vn>
2017-11-27 09:29:12 +0700

    add Anko lybrary

---
+++ app/src/main/java/vn/asiantech/way/ui/search/SearchActivity.kt
@@ -0,0 +1,10 @@
+package vn.asiantech.way.ui.search
+
+import vn.asiantech.way.ui.base.BaseActivity
+
+/**
+ * Copyright © 2017 Asian Tech Co., Ltd.
+ * Created by cuongcaov on 27/11/2017
+ */
+class SearchActivity : BaseActivity() {
+}
---
+++ app/src/main/java/vn/asiantech/way/ui/search/SearchActivityUI.kt
@@ -0,0 +1,8 @@
+package vn.asiantech.way.ui.search
+
+/**
+ * Copyright © 2017 Asian Tech Co., Ltd.
+ * Created by cuongcaov on 27/11/2017
+ */
+class SearchActivityUI {
+}
