f8fc11f9356d5b729ab28c5c582d8b8f642ec5d8
at-toannguyen1 <toannt2@asiantech.vn>
2017-11-27 16:36:38 +0700

    update

--- app/src/main/java/vn/asiantech/way/ui/splash/SplashActivityUI.kt
+++ app/src/main/java/vn/asiantech/way/ui/splash/SplashActivityUI.kt
@@ -29,21 +29,22 @@ class SplashActivityUI : AnkoComponent<SplashActivity> {
                 gravity = Gravity.CENTER_HORIZONTAL
                 imageView {
                     backgroundResource = R.drawable.ic_location_round
-                }.lparams(dip(250), dip(250)) {
-                    topMargin = dip(30)
+                }.lparams(dimen(R.dimen.splash_img_circle_with_high)
+                        , dimen(R.dimen.splash_img_circle_with_high)) {
+                    topMargin = dimen(R.dimen.splash_img_location_margin_top)
                 }
                 textView(R.string.splash_name_application) {
                     textSize = px2dip(dimen(R.dimen.splash_size_tv_app_name))
                     typeface = Typeface.DEFAULT_BOLD
                     textColor = Color.BLACK
                 }.lparams(wrapContent, wrapContent) {
-                    topMargin = dip(30)
+                    topMargin = dimen(R.dimen.splash_tv_app_name_margin_top)
                 }
                 textView(R.string.splash_description_app) {
                     textColor = Color.BLACK
                     textSize = px2dip(dimen(R.dimen.splash_tv_size_app_description))
                 }.lparams(wrapContent, wrapContent) {
-                    topMargin = dip(40)
+                    topMargin = dimen(R.dimen.splash_tv_description_margin_top)
                     leftMargin = dimen(R.dimen.splash_tv_description_margin)
                     rightMargin = dimen(R.dimen.splash_tv_description_margin)
                     gravity = Gravity.CENTER
@@ -59,7 +60,7 @@ class SplashActivityUI : AnkoComponent<SplashActivity> {
                     rightPadding = dimen(R.dimen.splash_buttonEnableButton_padding)
                     textColor = ContextCompat.getColor(context, R.color.colorWhite)
                 }.lparams(wrapContent, wrapContent) {
-                    topMargin = dip(30)
+                    topMargin = dimen(R.dimen.splash_btn_enable_location_margin_top)
                     bottomMargin = dimen(R.dimen.splash_progressBar_margin_bottom)
                 }
             }
