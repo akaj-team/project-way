2f7672d665cd6d38fbc01305db08f1cb778160c1
at-hoavo <hoa.vo@asiantech.vn>
2017-11-27 10:08:51 +0700

    fix fail ci

--- app/src/main/java/vn/asiantech/way/data/source/WayDataSource.kt
+++ app/src/main/java/vn/asiantech/way/data/source/WayDataSource.kt
@@ -12,28 +12,94 @@ import vn.asiantech.way.data.source.remote.response.LocationResponse
  */
 internal interface WayDataSource {
 
+    /**
+     *  This method to create new User.
+     *
+     *  @param userParams contain user information for Hypertrack to create new user.
+     */
     fun createUser(userParams:UserParams)
 
+    /**
+     *  This method to update User.
+     *
+     *  @param userParams: contain user information for Hypertrack to update.
+     */
     fun updateUser(userParams:UserParams)
 
+    /**
+     *  Get information user.
+     *
+     *  @return User object which is Observable.
+     */
     fun getUser():Observable<User>
 
+    /**
+     *  Get address at location on Map.
+     *
+     *  @param latLng: information of position.
+     *  @return LocationResponse object which is Observable.
+     */
     fun getAddressLocation(latLng: String): Observable<LocationResponse>
 
+    /**
+     *  This method to get detail of location.
+     *
+     *  @param placeId the id of location.
+     *  @return ResultPlaceDetail object which is Observable.
+     */
     fun getLocationDetail(placeId: String?, key: String): Observable<ResultPlaceDetail>
 
+    /**
+     *  This method to search location by name.
+     *
+     *  @param input the query to search location.
+     *  @param key the api key of google place api.
+     *  @return AutoCompleteResult object which is Observable.
+     */
     fun searchLocations(input: String,
                         key: String,
                         language: String = "vi",
                         sensor: Boolean = false): Observable<AutoCompleteResult>
 
+    /**
+     * This method used to call api create group.
+     *
+     * @param name - name of group you wanna create.
+     * @return Group object which is Observable.
+     */
     fun createGroup(name: String): Observable<Group>
 
+    /**
+     * This metod used to get information of a given group.
+     *
+     * @param groupId - id of given group.
+     * @return Group object which is Observable.
+     */
     fun getGroupInfo(groupId: String): Observable<Group>
 
+    /**
+     *  This method used to get members list of given group.
+     *
+     *  @param groupId - id of given group.
+     *  @return UserListResult object which is Observable.
+     */
     fun getMembersList(groupId: String): Observable<UserListResult>
 
+    /**
+     * This method used to add a user to given group.
+     *
+     * @param userId - id of  user you wanna add.
+     * @param body - data contain id of given group.
+     * @return User object which is Observable.
+     */
     fun addUserToGroup(userId: String, body: BodyAddUserToGroup): Observable<User>
 
+    /**
+     * This method used to remove a user from given group.
+     *
+     * @param userId - id of  user you wanna delete.
+     * @param body - data contain id of given group.
+     * @return User object which is Observable.
+     */
     fun removeUserFromGroup(userId: String, body: BodyAddUserToGroup): Observable<User>
 }
