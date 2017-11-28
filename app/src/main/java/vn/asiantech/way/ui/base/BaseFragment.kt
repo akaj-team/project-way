106b8146f44646304b61f71bedc1427aafb795ef
at-hoavo <hoa.vo@asiantech.vn>
2017-11-28 16:32:14 +0700

    add base fragment

---
+++ app/src/main/java/vn/asiantech/way/ui/base/BaseFragment.kt
@@ -0,0 +1,11 @@
+package vn.asiantech.way.ui.base
+
+import android.support.v4.app.Fragment
+
+/**
+ * Copyright © 2017 Asian Tech Co., Ltd.
+ * Created by quocnguyenp. on 9/21/17.
+ */
+abstract class BaseFragment : Fragment() {
+
+}
