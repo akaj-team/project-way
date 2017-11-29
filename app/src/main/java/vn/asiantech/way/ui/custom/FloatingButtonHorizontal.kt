74aa924500598c023b854dd7c77f73791e67697d
at-toannguyen1 <toannt2@asiantech.vn>
2017-11-29 11:43:48 +0700

    complete change customview floating button from xml to anko

--- app/src/main/java/vn/asiantech/way/ui/custom/FloatingButtonHorizontal.kt
+++ app/src/main/java/vn/asiantech/way/ui/custom/FloatingButtonHorizontal.kt
@@ -2,11 +2,18 @@ package vn.asiantech.way.ui.custom
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.Gravity
 import android.view.View
+import android.view.ViewManager
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import android.widget.ImageButton
 import android.widget.LinearLayout
-import kotlinx.android.synthetic.main.custom_floating_button_menu_horizontal.view.*
+import android.widget.RelativeLayout
+import android.widget.TextView
+import org.jetbrains.anko.*
+import org.jetbrains.anko.custom.ankoView
+import org.jetbrains.anko.sdk25.coroutines.onClick
 import vn.asiantech.way.R
 
 /**
@@ -15,71 +22,176 @@ import vn.asiantech.way.R
  */
 class FloatingButtonHorizontal(
         context: Context, attrs: AttributeSet? = null) :
-        LinearLayout(context, attrs), View.OnClickListener {
-    private lateinit var ui: FloatingButtonHorizontalUI
-
-    init {
-        ui= FloatingButtonHorizontalUI()
-        inflate(context, R.layout.custom_floating_button_menu_horizontal, this)
-        imgBtnMenu.setOnClickListener(this)
-        imgBtnShare.setOnClickListener(this)
-        imgBtnProfile.setOnClickListener(this)
-        imgBtnCalendar.setOnClickListener(this)
-        imgBtnSearch.setOnClickListener(this)
-        imgBtnGroup.setOnClickListener(this)
+        LinearLayout(context, attrs) {
+    companion object {
+        private const val ID_BTN_SEARCH = 1001
+        private const val ID_BTN_GROUP = 1002
+        private const val ID_BTN_CALENDAR = 1003
+        private const val ID_BTN_PROFILE = 1004
+        private const val ID_BTN_SHARE = 1005
     }
+
+    private lateinit var rlSearch: RelativeLayout
+    private lateinit var imgBtnSearch: ImageButton
+    private lateinit var tvSearchTitle: TextView
+    private lateinit var rlGroup: RelativeLayout
+    private lateinit var imgBtnGroup: ImageButton
+    private lateinit var tvGroupTitle: TextView
+    private lateinit var rlCalendar: RelativeLayout
+    private lateinit var imgBtnCalendar: ImageButton
+    private lateinit var tvCalendarTitle: TextView
+    private lateinit var rlProfile: RelativeLayout
+    private lateinit var imgBtnProfile: ImageButton
+    private lateinit var tvProfileTitle: TextView
+    private lateinit var rlShare: RelativeLayout
+    private lateinit var imgBtnShare: ImageButton
+    private lateinit var tvShareTitle: TextView
+    private lateinit var imgBtnMenu: ImageButton
 
     private var mOnMenuClickListener: OnMenuClickListener? = null
 
-    override fun onClick(view: View?) {
-        when (view?.id) {
-            R.id.imgBtnMenu -> {
-                val anim: Animation = AnimationUtils.loadAnimation(context, R.anim.anim_rotate)
-                val animVisible: Animation = AnimationUtils.loadAnimation(context, R.anim.anim_visible)
-                val animInvisible: Animation = AnimationUtils.loadAnimation(context, R.anim.anim_invisible)
-                imgBtnMenu.startAnimation(anim)
-                if (rlShare.visibility == View.INVISIBLE) {
-                    startAnimationFab(animVisible)
-                    visibilityAllChildView(View.VISIBLE)
-                    mOnMenuClickListener?.onMenuClick(true)
-                } else {
-                    startAnimationFab(animInvisible)
-                    mOnMenuClickListener?.onMenuClick(false)
+    init {
+        AnkoContext.createDelegate(this).apply {
+            verticalLayout {
+                lparams(wrapContent, wrapContent)
+                rlSearch = relativeLayout {
+                    imgBtnSearch = imageButton {
+                        id = ID_BTN_SEARCH
+                        imageResource = R.drawable.ic_search
+                        backgroundResource = R.drawable.custom_bg_item_search_button
+                        onClick {
+                            visibilityAllChildView(View.INVISIBLE)
+                            mOnMenuClickListener?.onSearchClick()
+                        }
+                    }.lparams(dimen(R.dimen.width_height_image_button), dimen(R.dimen.width_height_image_button)) {
+                        alignParentRight()
+                        rightMargin = dimen(R.dimen.margin_right_image_button)
+                    }
+                    tvSearchTitle = textView(R.string.custom_floating_menu_search_title) {
+                        backgroundResource = R.drawable.custom_bg_item_menu_title
+                    }.lparams(wrapContent, dimen(R.dimen.height_text_view)) {
+                        centerVertically()
+                        rightMargin = dimen(R.dimen.margin_right_text_view)
+                        leftOf(ID_BTN_SEARCH)
+                        gravity = Gravity.CENTER
+                    }
+                }.lparams(dimen(R.dimen.width_relative_layout), wrapContent)
+                rlGroup = relativeLayout {
+                    imgBtnGroup = imageButton {
+                        id = ID_BTN_GROUP
+                        imageResource = R.drawable.ic_group_white_24dp
+                        backgroundResource = R.drawable.custom_bg_item_search_button
+                        onClick {
+                            visibilityAllChildView(View.INVISIBLE)
+                            mOnMenuClickListener?.onGroupClick()
+                        }
+                    }.lparams(dimen(R.dimen.width_height_image_button), dimen(R.dimen.width_height_image_button)) {
+                        alignParentRight()
+                        rightMargin = dimen(R.dimen.margin_right_image_button)
+                    }
+                    tvGroupTitle = textView(R.string.custom_floating_menu_group_title) {
+                        backgroundResource = R.drawable.custom_bg_item_menu_title
+                    }.lparams(wrapContent, dimen(R.dimen.height_text_view)) {
+                        centerVertically()
+                        rightMargin = dimen(R.dimen.margin_right_text_view)
+                        leftOf(ID_BTN_GROUP)
+                        gravity = Gravity.CENTER
+                    }
+                }.lparams(dimen(R.dimen.width_relative_layout), wrapContent)
+                rlCalendar = relativeLayout {
+                    imgBtnCalendar = imageButton {
+                        id = ID_BTN_CALENDAR
+                        backgroundResource = R.drawable.custom_bg_item_calendar_button
+                        imageResource = R.drawable.ic_calendar
+                        onClick {
+                            visibilityAllChildView(View.INVISIBLE)
+                            mOnMenuClickListener?.onCalendarClick()
+                        }
+                    }.lparams(dimen(R.dimen.width_height_image_button), dimen(R.dimen.width_height_image_button)) {
+                        alignParentRight()
+                        rightMargin = dimen(R.dimen.margin_right_image_button)
+                    }
+                    tvCalendarTitle = textView(R.string.custom_floating_menu_calendar_title) {
+
+                        backgroundResource = R.drawable.custom_bg_item_menu_title
+                    }.lparams(wrapContent, dimen(R.dimen.height_text_view)) {
+                        centerVertically()
+                        rightMargin = dimen(R.dimen.margin_right_text_view)
+                        leftOf(ID_BTN_CALENDAR)
+                        gravity = Gravity.CENTER
+                    }
+                }.lparams(dimen(R.dimen.width_relative_layout), wrapContent)
+                rlProfile = relativeLayout {
+                    imgBtnProfile = imageButton {
+                        id = ID_BTN_PROFILE
+                        imageResource = R.drawable.ic_profile
+                        backgroundResource = R.drawable.custom_bg_item_profile_button
+                        onClick {
+                            visibilityAllChildView(View.INVISIBLE)
+                            mOnMenuClickListener?.onProfileClick()
+                        }
+                    }.lparams(dimen(R.dimen.width_height_image_button), dimen(R.dimen.width_height_image_button)) {
+                        alignParentRight()
+                        rightMargin = dimen(R.dimen.margin_right_image_button)
+                    }
+                    tvProfileTitle = textView(R.string.custom_floating_menu_search_title) {
+                        backgroundResource = R.drawable.custom_bg_item_menu_title
+                    }.lparams(wrapContent, dimen(R.dimen.height_text_view)) {
+                        centerVertically()
+                        rightMargin = dimen(R.dimen.margin_right_text_view)
+                        leftOf(ID_BTN_PROFILE)
+                        gravity = Gravity.CENTER
+                    }
+                }.lparams(dimen(R.dimen.width_relative_layout), wrapContent)
+                rlShare = relativeLayout {
+                    imgBtnShare = imageButton {
+                        id = ID_BTN_SHARE
+                        imageResource = R.drawable.ic_share
+                        backgroundResource = R.drawable.custom_bg_item_share_button
+                        onClick {
+                            visibilityAllChildView(View.INVISIBLE)
+                            mOnMenuClickListener?.onShareClick()
+                        }
+                    }.lparams(dimen(R.dimen.width_height_image_button), dimen(R.dimen.width_height_image_button)) {
+                        alignParentRight()
+                        rightMargin = dimen(R.dimen.margin_right_image_button)
+                    }
+                    tvShareTitle = textView(R.string.custom_floating_menu_search_title) {
+                        backgroundResource = R.drawable.custom_bg_item_menu_title
+                    }.lparams(wrapContent, dimen(R.dimen.height_text_view)) {
+                        centerVertically()
+                        rightMargin = dimen(R.dimen.margin_right_text_view)
+                        leftOf(ID_BTN_SHARE)
+                        gravity = Gravity.CENTER
+                    }
+                }.lparams(dimen(R.dimen.width_relative_layout), wrapContent)
+                imgBtnMenu = imageButton {
+                    imageResource = R.drawable.ic_menu
+                    backgroundResource = R.drawable.custom_menu_button
+                    onClick {
+                        onClickMenu()
+                    }
+                }.lparams(dimen(R.dimen.width_height_image_button_menu)
+                        , dimen(R.dimen.width_height_image_button_menu)) {
+                    gravity = Gravity.END
+                    bottomMargin = dimen(R.dimen.top_bot_margin_image_button_menu)
+                    topMargin = dimen(R.dimen.top_bot_margin_image_button_menu)
+                    padding = dimen(R.dimen.padding_floating)
                 }
-                animInvisible.setAnimationListener(object : Animation.AnimationListener {
-                    override fun onAnimationRepeat(p0: Animation?) {
-                        // No-op
+            }.applyRecursively { view: View ->
+                when (view) {
+                    is RelativeLayout -> {
+                        view.gravity = Gravity.END
+                        view.visibility = View.INVISIBLE
                     }
+                    is TextView -> {
+                        view.leftPadding = dimen(R.dimen.padding_floating)
+                        view.rightPadding = dimen(R.dimen.padding_floating)
+                        view.textSize = px2dip(dimen(R.dimen.custom_menu_text_size))
+                    }
+                }
+            }
 
-                    override fun onAnimationEnd(p0: Animation?) {
-                        visibilityAllChildView(View.INVISIBLE)
-                    }
-
-                    override fun onAnimationStart(p0: Animation?) {
-                        // No-op
-                    }
-                })
-            }
-            R.id.imgBtnShare -> {
-                visibilityAllChildView(View.INVISIBLE)
-                mOnMenuClickListener?.onShareClick()
-            }
-            R.id.imgBtnProfile -> {
-                visibilityAllChildView(View.INVISIBLE)
-                mOnMenuClickListener?.onProfileClick()
-            }
-            R.id.imgBtnCalendar -> {
-                visibilityAllChildView(View.INVISIBLE)
-                mOnMenuClickListener?.onCalendarClick()
-            }
-            R.id.imgBtnSearch -> {
-                visibilityAllChildView(View.INVISIBLE)
-                mOnMenuClickListener?.onSearchClick()
-            }
-            R.id.imgBtnGroup -> {
-                visibilityAllChildView(View.INVISIBLE)
-                mOnMenuClickListener?.onGroupClick()
-            }
         }
     }
 
@@ -90,6 +202,34 @@ class FloatingButtonHorizontal(
         val animInvisible: Animation = AnimationUtils.loadAnimation(context, R.anim.anim_invisible)
         startAnimationFab(animInvisible)
         visibilityAllChildView(View.INVISIBLE)
+    }
+
+    private fun onClickMenu() {
+        val anim: Animation = AnimationUtils.loadAnimation(context, R.anim.anim_rotate)
+        val animVisible: Animation = AnimationUtils.loadAnimation(context, R.anim.anim_visible)
+        val animInvisible: Animation = AnimationUtils.loadAnimation(context, R.anim.anim_invisible)
+        imgBtnMenu.startAnimation(anim)
+        if (rlShare.visibility == View.INVISIBLE) {
+            startAnimationFab(animVisible)
+            visibilityAllChildView(View.VISIBLE)
+            mOnMenuClickListener?.onMenuClick(true)
+        } else {
+            startAnimationFab(animInvisible)
+            mOnMenuClickListener?.onMenuClick(false)
+        }
+        animInvisible.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(p0: Animation?) {
+                // No-op
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                visibilityAllChildView(View.INVISIBLE)
+            }
+
+            override fun onAnimationStart(p0: Animation?) {
+                // No-op
+            }
+        })
     }
 
     private fun startAnimationFab(animation: Animation) {
@@ -150,4 +290,9 @@ class FloatingButtonHorizontal(
          */
         fun onGroupClick()
     }
+}
+
+inline fun ViewManager.floatingButton(init: FloatingButtonHorizontal.() -> Unit):
+        FloatingButtonHorizontal {
+    return ankoView({ FloatingButtonHorizontal(it, null) }, theme = 0, init = init)
 }
