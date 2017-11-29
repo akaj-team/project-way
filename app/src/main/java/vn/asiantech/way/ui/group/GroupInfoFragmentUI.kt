692d3d313b30ec82036356a332566e6a6b098552
at-haingo <hai.ngo@asiantech.vn>
2017-11-29 10:57:07 +0700

    remove unused import

--- app/src/main/java/vn/asiantech/way/ui/group/GroupInfoFragmentUI.kt
+++ app/src/main/java/vn/asiantech/way/ui/group/GroupInfoFragmentUI.kt
@@ -1,7 +1,6 @@
 package vn.asiantech.way.ui.group
 
 import android.graphics.Color
-import android.support.v4.app.ActivityCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.Gravity
