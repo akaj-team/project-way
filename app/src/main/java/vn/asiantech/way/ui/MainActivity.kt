eef16fce2f02c90ad685734f5f4326a728765105
Cuong Cao V <cuong.cao@asiantech.vn>
2017-11-29 10:39:51 +0700

    fix convention

--- app/src/main/java/vn/asiantech/way/ui/MainActivity.kt
+++ app/src/main/java/vn/asiantech/way/ui/MainActivity.kt
@@ -1,13 +1,16 @@
 package vn.asiantech.way.ui
 
+import android.content.Intent
 import android.os.Bundle
 import vn.asiantech.way.R
 import vn.asiantech.way.ui.base.BaseActivity
+import vn.asiantech.way.ui.search.SearchActivity
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this, SearchActivity::class.java))
     }
 }
