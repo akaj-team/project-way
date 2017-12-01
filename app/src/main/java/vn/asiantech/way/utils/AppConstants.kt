4675db85998f4bbe9d1281366e7f3050ed28ba35
Cuong Cao V <cuong.cao@asiantech.vn>
2017-12-01 16:36:54 +0700

    update AppConstants file

--- app/src/main/java/vn/asiantech/way/utils/AppConstants.kt
+++ app/src/main/java/vn/asiantech/way/utils/AppConstants.kt
@@ -5,20 +5,20 @@ package vn.asiantech.way.utils
  * Created by datbuit. on 24/10/2017.
  */
 object AppConstants {
-    val KEY_LOCATION = "location"
-    val KEY_CONFIRM = "confirm"
-    val KEY_CURRENT_LOCATION = "current_location"
-    val KEY_SHARING = "sharing"
-    val KEY_START_SHARING = "start_sharing"
-    val KEY_DEFAULT_ANCHOR = 0.5f
-    val KEY_DRAWABLE_SIZE =  500
-    val KEY_GROUND_OVERLAY_POSITION = 500f
-    val KEY_GR_ANIMATION_DUR: Long = 2000L
-    val GOOGLE_MAP_API_KEY = "AIzaSyDcXFSC2I6ZqxQeAbUMFvJKMrA98217H9U"
-    val KEY_SEARCH_SCREEN_WAY_LOCATION_HISTORY = "key_history"
-    val SEARCH_SCREEN_HISTORY_MAX_SIZE = 10
-    val WAITING_TIME_FOR_SEARCH_FUNCTION = 300L
-    val ACTION_CHOOSE_ON_MAP = "action_choose_on_map"
-    val ACTION_CURRENT_LOCATION = "action_current_location"
-    val ACTION_SEND_WAY_LOCATION = "action_send_way_location"
+    const val KEY_LOCATION = "location"
+    const val KEY_CONFIRM = "confirm"
+    const val KEY_CURRENT_LOCATION = "current_location"
+    const val KEY_SHARING = "sharing"
+    const val KEY_START_SHARING = "start_sharing"
+    const val KEY_DEFAULT_ANCHOR = 0.5f
+    const val KEY_DRAWABLE_SIZE = 500
+    const val KEY_GROUND_OVERLAY_POSITION = 500f
+    const val KEY_GR_ANIMATION_DUR: Long = 2000L
+    const val GOOGLE_MAP_API_KEY = "AIzaSyDcXFSC2I6ZqxQeAbUMFvJKMrA98217H9U"
+    const val KEY_SEARCH_SCREEN_WAY_LOCATION_HISTORY = "key_history"
+    const val SEARCH_SCREEN_HISTORY_MAX_SIZE = 10
+    const val WAITING_TIME_FOR_SEARCH_FUNCTION = 300L
+    const val ACTION_CHOOSE_ON_MAP = "action_choose_on_map"
+    const val ACTION_CURRENT_LOCATION = "action_current_location"
+    const val ACTION_SEND_WAY_LOCATION = "action_send_way_location"
 }
