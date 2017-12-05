657155a0ee8e3b123b5643039d14fd808347cece
at-toannguyen1 <toannt2@asiantech.vn>
2017-12-05 10:25:23 +0700

    update logic create group

--- app/src/main/java/vn/asiantech/way/ui/group/CreateGroupFragment.kt
+++ app/src/main/java/vn/asiantech/way/ui/group/CreateGroupFragment.kt
@@ -2,8 +2,11 @@ package vn.asiantech.way.ui.group
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.support.v4.toast
+import vn.asiantech.way.extension.observeOnUiThread
 import vn.asiantech.way.ui.base.BaseFragment
 
 /**
@@ -11,11 +14,32 @@ import vn.asiantech.way.ui.base.BaseFragment
  * Created by haingoq on 28/11/2017.
  */
 class CreateGroupFragment : BaseFragment() {
+    lateinit var createGroupViewModel: CreateGroupViewModel
+    lateinit var ui: CreateGroupFragmentUI
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?) = CreateGroupFragmentUI()
             .createView(AnkoContext.create(context, this))
 
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        ui = CreateGroupFragmentUI()
+        createGroupViewModel = CreateGroupViewModel(activity)
+    }
+
     override fun onBindViewModel() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    /**
+     * Create Group
+     */
+    internal fun createGroup() {
+        addDisposables(createGroupViewModel.createGroup(ui.edtGroupName.text.toString())
+                .observeOnUiThread()
+                .subscribe({
+                    toast("Create Group Success")
+                }, {
+                    toast("$it")
+                }))
     }
 }
---
+++ app/src/main/java/vn/asiantech/way/ui/group/CreateGroupViewModel.kt
@@ -0,0 +1,19 @@
+package vn.asiantech.way.ui.group
+
+import android.content.Context
+import io.reactivex.Observable
+import vn.asiantech.way.data.model.Group
+import vn.asiantech.way.data.source.WayRepository
+
+/**
+ * CreateGroupViewModel.
+ *
+ * @author at-ToanNguyen
+ */
+class CreateGroupViewModel(val context: Context) {
+    private val wayRepository = WayRepository()
+
+    internal fun createGroup(name: String): Observable<Group> {
+        return wayRepository.createGroup(name)
+    }
+}
