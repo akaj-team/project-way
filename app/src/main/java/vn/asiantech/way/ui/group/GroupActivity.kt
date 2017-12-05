697317d626d55aea164a013f28f50f4aec33ee5a
Cuong Cao V <cuong.cao@asiantech.vn>
2017-12-05 11:18:05 +0700

    revert GroupActivity

--- app/src/main/java/vn/asiantech/way/ui/group/GroupActivity.kt
+++ app/src/main/java/vn/asiantech/way/ui/group/GroupActivity.kt
@@ -1,11 +1,7 @@
 package vn.asiantech.way.ui.group
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.util.Log
-import com.hypertrack.lib.models.User
 import org.jetbrains.anko.setContentView
-import vn.asiantech.way.R
 import vn.asiantech.way.ui.base.BaseActivity
 
 /**
@@ -16,7 +12,6 @@ class GroupActivity : BaseActivity() {
 
     private lateinit var ui: GroupActivityUI
     private val groupViewModel = GroupActivityViewModel()
-    private var currentGroupId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,33 +21,5 @@ class GroupActivity : BaseActivity() {
 
     override fun onBindViewModel() {
         // TODO: handle later
-        addDisposables(
-                groupViewModel.getUser().subscribe {
-                    this::afterGetUser
-                },
-                groupViewModel.getGroupId("\"e4e91b20-498b-49a0-b2aa-64b9a992e21d\"")
-                        .subscribe {
-                            groupViewModel.getUser()
-                                    .subscribe {
-                                        if (currentGroupId != null && it.groupId != currentGroupId) {
-                                            currentGroupId = it.groupId
-                                            Log.i("tag11", "reload")
-                                        }
-                                    }
-                        }
-        )
-    }
-
-    private fun replaceFragment(fragment: Fragment) {
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.group_activity_ui_fr_content, fragment)
-        transaction.commit()
-    }
-
-    private fun afterGetUser(user: User) {
-        if (user.groupId != null) {
-            currentGroupId = user.groupId
-            Log.i("tag11", "show")
-        }
     }
 }
