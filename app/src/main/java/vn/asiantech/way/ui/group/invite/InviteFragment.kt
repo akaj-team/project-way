cdb44fc4ebcb2d7f9de93c0ee27b6c4fe96625f3
ngoctrant.n <ngoc.tran@asiantech.vn>
2017-12-05 14:14:46 +0700

    get list

--- app/src/main/java/vn/asiantech/way/ui/group/invite/InviteFragment.kt
+++ app/src/main/java/vn/asiantech/way/ui/group/invite/InviteFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.hypertrack.lib.models.User
 import org.jetbrains.anko.AnkoContext
+import vn.asiantech.way.extension.observeOnUiThread
 import vn.asiantech.way.ui.base.BaseFragment
 
 /**
