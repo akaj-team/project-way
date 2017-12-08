ac9c2bbd7695b9455239815ddcd2821ac1c6fe9d
Cuong Cao V <cuong.cao@asiantech.vn>
2017-12-08 11:03:09 +0700

    revert CreateGroupViewModel

--- app/src/main/java/vn/asiantech/way/ui/group/CreateGroupViewModel.kt
+++ app/src/main/java/vn/asiantech/way/ui/group/CreateGroupViewModel.kt
@@ -1,7 +1,11 @@
 package vn.asiantech.way.ui.group
 
-import io.reactivex.Single
+import com.hypertrack.lib.models.User
+import io.reactivex.Observable
+import vn.asiantech.way.data.model.BodyAddUserToGroup
+import vn.asiantech.way.data.model.Group
 import vn.asiantech.way.data.source.GroupRepository
+import vn.asiantech.way.data.source.WayRepository
 
 /**
  * CreateGroupViewModel.
@@ -9,9 +13,23 @@ import vn.asiantech.way.data.source.GroupRepository
  * @author at-ToanNguyen
  */
 class CreateGroupViewModel {
+    private val wayRepository = WayRepository()
     private val groupRepository = GroupRepository()
 
-    internal fun createGroup(name: String, userId: String): Single<Boolean> {
-        return groupRepository.createGroup(name, userId)
+    internal fun createGroup(name: String, userId: String): Observable<Boolean> {
+        return wayRepository.createGroup(name).flatMap { group -> addUserToGroup(userId, group) }.flatMap { postGroupInfo(it) }
+    }
+
+    private fun addUserToGroup(userId: String, body: BodyAddUserToGroup): Observable<User> {
+        return wayRepository.addUserToGroup(userId, body)
+    }
+
+    private fun addUserToGroup(userId: String, group: Group): Observable<Group> {
+        group.ownerId = userId
+        return addUserToGroup(userId, BodyAddUserToGroup(group.id)).map { group }
+    }
+
+    private fun postGroupInfo(group: Group): Observable<Boolean> {
+        return groupRepository.postGroupInfo(group)
     }
 }
