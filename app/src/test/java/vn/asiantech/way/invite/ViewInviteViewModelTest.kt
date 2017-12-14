7fcf4b201737c5accd02b8794cc0a519212c0a87
at-toannguyen1 <toannt2@asiantech.vn>
2017-12-14 17:34:22 +0700

    optimize code

--- app/src/test/java/vn/asiantech/way/invite/ViewInviteViewModelTest.kt
+++ app/src/test/java/vn/asiantech/way/invite/ViewInviteViewModelTest.kt
@@ -1,4 +1,4 @@
-package vn.asiantech.way
+package vn.asiantech.way.invite
 
 import io.reactivex.Observable
 import io.reactivex.Single
@@ -8,6 +8,7 @@ import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
+import vn.asiantech.way.TestUtil
 import vn.asiantech.way.data.model.Invite
 import vn.asiantech.way.data.source.GroupRepository
 import vn.asiantech.way.ui.group.showinvite.ViewInviteViewModel
@@ -22,40 +23,53 @@ class ViewInviteViewModelTest {
     @Mock
     private lateinit var groupRepository: GroupRepository
     private lateinit var viewModel: ViewInviteViewModel
-    private lateinit var userId: String
     @Before
     fun initTest() {
-        userId = ""
         MockitoAnnotations.initMocks(this)
-        viewModel = ViewInviteViewModel(userId, groupRepository)
+        viewModel = ViewInviteViewModel("", groupRepository)
     }
 
     @Test
     fun `Given invites of user - When call get invites of user - then return right invite`() {
+        /* Given */
         val inviteTest = TestObserver<Invite>()
         val invite = Invite("from", "to", "groupName", true)
         `when`(groupRepository.getInvite(TestUtil.any())).thenReturn(Observable.just(invite))
+
+        /* When */
         viewModel.getInvitesOfUser().subscribe(inviteTest)
+
+        /* Then */
         inviteTest.assertValue { it == invite }
     }
 
     @Test
-    fun ` Given a invite - When call remove invite user from group - then return progressbar show then hide`() {
+    fun `Given a invite - When call remove invite user from group - then return progressbar show then hide`() {
+        /* Given */
         val statusTest = TestObserver<Boolean>()
         val invite = Invite("from", "to", "groupName", true)
         `when`(groupRepository.deleteUserInvite(TestUtil.any(), TestUtil.any())).thenReturn(Single.just(true))
+
+        /* When */
         viewModel.progressDialogObservable.subscribe(statusTest)
         viewModel.removeInviteUserFromGroup(invite).subscribe()
+
+        /* Then */
         statusTest.assertValues(true, false)
     }
 
     @Test
     fun `Given a invite user - When call accept invite - then return progressbar show then hide`() {
+        /* Given */
         val statusTest = TestObserver<Boolean>()
         val invite = Invite("from", "to", "groupName", true)
         `when`(groupRepository.acceptInvite(TestUtil.any(), TestUtil.any())).thenReturn(Single.just(true))
+
+        /* When */
         viewModel.progressDialogObservable.subscribe(statusTest)
         viewModel.acceptInvite(invite).subscribe()
+
+        /* Then */
         statusTest.assertValues(true, false)
     }
 }
