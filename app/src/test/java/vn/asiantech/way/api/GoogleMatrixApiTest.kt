d077fb4fe398daae431e05fa90814d442178b220
at-datbui <dat.bui@asiantech.vn>
2017-12-18 16:37:24 +0700

    add api test

---
+++ app/src/test/java/vn/asiantech/way/api/GoogleMatrixApiTest.kt
@@ -0,0 +1,61 @@
+package vn.asiantech.way.api
+
+import io.reactivex.observers.TestObserver
+import okhttp3.mockwebserver.MockWebServer
+import org.hamcrest.CoreMatchers
+import org.junit.*
+import org.junit.runners.MethodSorters
+import vn.asiantech.way.RestClient
+import vn.asiantech.way.data.source.remote.googleapi.ApiService
+import vn.asiantech.way.extension.addResponseBody
+
+/**
+ * Copyright © 2017 Asian Tech Co., Ltd.
+ * Created by datbuit. on 18/12/2017.
+ */
+@Suppress("IllegalIdentifier")
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class GoogleMatrixApiTest {
+    private val server = MockWebServer()
+    private lateinit var restClient: ApiService
+
+    @Before
+    fun initTests() {
+        server.start()
+        val baseUrl = server.url("/maps/api/")
+        restClient = RestClient.getClient(baseUrl, ApiService::class.java)
+    }
+
+    @After
+    fun cleanUp() {
+        server.shutdown()
+    }
+
+    @Test
+    fun `Given mock two lat lng point - When request google distance matrix api - Then return resultDistance object`() {
+        /* Given */
+        val test = TestObserver<ResultDistance>()
+        server.addResponseBody("locationDistance.json")
+
+        /* When */
+        restClient.getLocationDistance("16.083833,108.243501", "16.084153,108.242214").subscribe(test)
+
+        /* Then */
+        val request = server.takeRequest()
+        Assert.assertThat(request.method.toUpperCase(), CoreMatchers.`is`("GET"))
+        Assert.assertThat(request.requestUrl.queryParameterNames(), CoreMatchers.hasItems("units", "origins", "destinations"))
+        Assert.assertThat(request.requestUrl.queryParameter("origins"), CoreMatchers.`is`("16.083833,108.243501"))
+        Assert.assertThat(request.requestUrl.queryParameter("destinations"), CoreMatchers.`is`("16.084153,108.242214"))
+
+        test.assertValue {
+            val item = it.rows[0].elements[0]
+            Assert.assertThat(it.rows.size, CoreMatchers.`is`(1))
+            Assert.assertThat(item.distance.text, CoreMatchers.`is`("0.4 km"))
+            Assert.assertThat(item.distance.value, CoreMatchers.`is`(351))
+            Assert.assertThat(item.duration.text, CoreMatchers.`is`("1 min"))
+            Assert.assertThat(item.duration.value, CoreMatchers.`is`(76))
+
+            it.rows.isNotEmpty()
+        }
+    }
+}
---
+++ app/src/test/java/vn/asiantech/way/api/GoogleSnapToRoadApiTest.kt
@@ -0,0 +1,64 @@
+package vn.asiantech.way.api
+
+import io.reactivex.observers.TestObserver
+import okhttp3.mockwebserver.MockWebServer
+import org.hamcrest.CoreMatchers
+import org.junit.*
+import org.junit.runners.MethodSorters
+import vn.asiantech.way.RestClient
+import vn.asiantech.way.data.source.remote.googleapi.ApiService
+import vn.asiantech.way.extension.addResponseBody
+
+/**
+ * Copyright © 2017 Asian Tech Co., Ltd.
+ * Created by datbuit. on 18/12/2017.
+ */
+@Suppress("IllegalIdentifier")
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class GoogleSnapToRoadApiTest {
+    private val server = MockWebServer()
+    private lateinit var restClient: ApiService
+
+    @Before
+    fun initTests() {
+        server.start()
+        val baseUrl = server.url("")
+        restClient = RestClient.getClient(baseUrl, ApiService::class.java)
+    }
+
+    @After
+    fun cleanUp() {
+        server.shutdown()
+    }
+
+    @Test
+    fun `Given mock an url - When request google road api - Then return resultRoad object`() {
+        /* Given */
+        val test = TestObserver<ResultRoad>()
+        val url = "roads.googleapis.com/v1/snapToRoads?path=16.0798071,108.2364393|16.0803531,108.2354526&interpolate=true&key=AIzaSyCZc4PAEpeVC18QnS5fPBt5hk3EFMbFjj8"
+        server.addResponseBody("googleSnapToRoad.json")
+
+        /* When */
+        restClient.getListLocation(url).subscribe(test)
+
+        /* Then */
+        val request = server.takeRequest()
+        Assert.assertThat(request.method.toUpperCase(), CoreMatchers.`is`("GET"))
+        Assert.assertThat(request.requestUrl.queryParameterNames(), CoreMatchers.hasItems("path", "interpolate", "key"))
+        Assert.assertThat(request.requestUrl.queryParameter("path"), CoreMatchers.`is`("16.0798071,108.2364393|16.0803531,108.2354526"))
+        Assert.assertThat(request.requestUrl.queryParameter("interpolate"), CoreMatchers.`is`("true"))
+        Assert.assertThat(request.requestUrl.queryParameter("key"), CoreMatchers.`is`("AIzaSyCZc4PAEpeVC18QnS5fPBt5hk3EFMbFjj8"))
+
+        test.assertValue {
+            Assert.assertThat(it.locationRoads.size, CoreMatchers.`is`(9))
+            Assert.assertThat(it.locationRoads[0].point.latitude, CoreMatchers.`is`(16.079881331167627))
+            Assert.assertThat(it.locationRoads[0].point.longitude, CoreMatchers.`is`(108.23634932975393))
+            Assert.assertThat(it.locationRoads[0].placeId, CoreMatchers.`is`("ChIJVXmLIycYQjERpY4bM9aTUG8"))
+            Assert.assertThat(it.locationRoads[8].point.latitude, CoreMatchers.`is`(16.080934072276641))
+            Assert.assertThat(it.locationRoads[8].point.longitude, CoreMatchers.`is`(108.23596334365941))
+            Assert.assertThat(it.locationRoads[8].placeId, CoreMatchers.`is`("ChIJnXA9wiAYQjERQ1CTfr7tnSE"))
+
+            it.locationRoads.isNotEmpty()
+        }
+    }
+}
