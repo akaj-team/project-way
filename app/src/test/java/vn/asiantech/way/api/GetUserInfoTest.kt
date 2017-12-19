b32475ef8d766957ddfc52fb4822c79bd1025398
Hao Le K <hao.le2@asiantech.vn>
2017-12-19 11:19:26 +0700

    fix comments

--- app/src/test/java/vn/asiantech/way/api/GetUserInfoTest.kt
+++ app/src/test/java/vn/asiantech/way/api/GetUserInfoTest.kt
@@ -7,6 +7,7 @@ import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.hasItems
 import org.junit.After
 import org.junit.Assert
+import org.junit.Assert.assertThat
 import org.junit.Before
 import org.junit.Test
 import vn.asiantech.way.data.source.remote.hypertrackapi.HypertrackService
@@ -48,8 +49,12 @@ class GetUserInfoTest {
         Assert.assertThat(request.method.toUpperCase(), `is`("GET"))
         Assert.assertThat(request.requestUrl.pathSegments(), hasItems("users", "{userId}"))
 
-        test.assertValue { it.id == "a7f13570-5e5e-4651-a58d-985bfcfcedf7" }
-        test.assertValue { it.groupId == "5bb1367e-8941-4874-8897-0bb84f154bfa" }
-        test.assertValue { it.name == "rim" }
+        test.assertValue {
+            assertThat(it.id, `is`("a7f13570-5e5e-4651-a58d-985bfcfcedf7"))
+            assertThat(it.groupId, `is`("5bb1367e-8941-4874-8897-0bb84f154bfa"))
+            assertThat(it.name, `is`("rim"))
+            true
+        }
     }
+
 }
