45f2446aa1e4a583c57e0c199128a47ec25ad2bc
at-hoavo <hoa.vo@asiantech.vn>
2017-12-19 10:00:31 +0700

    fix api test and diff

--- app/src/test/java/vn/asiantech/way/api/HypertrackApiTest.kt
+++ app/src/test/java/vn/asiantech/way/api/HypertrackApiTest.kt
@@ -3,7 +3,8 @@ package vn.asiantech.way.api
 import com.hypertrack.lib.models.User
 import io.reactivex.observers.TestObserver
 import okhttp3.mockwebserver.MockWebServer
-import org.hamcrest.CoreMatchers.*
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.hasItems
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.After
 import org.junit.Before
@@ -58,6 +59,7 @@ class HypertrackApiTest {
             assertThat(it.groups.size, `is`(2))
             assertThat(item.id, `is`("83892b6f-4d4e-4ead-bcdd-d95a24571506"))
             assertThat(item.name, `is`("Group Cua Cuong"))
+            print(item.createAt)
             assertThat(item.createAt, `is`("2017-11-09T04:02:01.769935Z"))
             assertThat(item.modifiedAt, `is`("2017-11-09T04:02:01.784838Z"))
             assertThat(item.token, `is`("gk_f1a6f81c400dc67bacfd547dfd66aa16b08f8191"))
@@ -77,6 +79,7 @@ class HypertrackApiTest {
 
         /* Then */
         val request = server.takeRequest()
+
         print(request.body.toString())
         assertThat(request.method.toUpperCase(), `is`("PATCH"))
         assertThat(request.requestUrl.pathSegments(), hasItems("userId"))
@@ -85,8 +88,8 @@ class HypertrackApiTest {
         testRemoveUserGroup.assertValue {
             assertThat(it.id, `is`("8f525af5-05fa-44a3-8c6e-d7a0180e259f"))
             assertThat(it.name, `is`("haiiiiaaa"))
-            assertThat(it.lookupId,`is`("123456"))
-            assertThat(it.photo,`is`("https://core-api-prod-assets.s3.amazonaws.com/uploads/photos/drivers/driver_0df3cd1e-82ce-44f5-918e-d41884a8892b_image_name"))
+            assertThat(it.lookupId, `is`("123456"))
+            assertThat(it.photo, `is`("https://core-api-prod-assets.s3.amazonaws.com/uploads/photos/drivers/driver_0df3cd1e-82ce-44f5-918e-d41884a8892b_image_name"))
             true
         }
     }
--- app/src/test/java/vn/asiantech/way/group/search/SearchGroupViewModelTest.kt
+++ app/src/test/java/vn/asiantech/way/group/search/SearchGroupViewModelTest.kt
@@ -1,18 +1,24 @@
 package vn.asiantech.way.group.search
 
+import android.support.v7.util.DiffUtil
+import android.support.v7.util.ListUpdateCallback
 import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.observers.TestObserver
+import org.hamcrest.CoreMatchers
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
-import vn.asiantech.way.util.TestUtil
 import vn.asiantech.way.data.model.Group
 import vn.asiantech.way.data.model.Invite
 import vn.asiantech.way.data.source.GroupRepository
 import vn.asiantech.way.ui.group.search.SearchGroupViewModel
+import vn.asiantech.way.util.RxSchedulersOverrideRule
+import vn.asiantech.way.util.TestUtil
 
 /**
  *  Copyright © 2017 AsianTech inc.
@@ -25,6 +31,9 @@ class SearchGroupViewModelTest {
     private lateinit var groupRepository: GroupRepository
     private lateinit var viewModel: SearchGroupViewModel
 
+    @get:Rule
+    val rule1 = RxSchedulersOverrideRule()
+
     @Before
     fun initTest() {
         MockitoAnnotations.initMocks(this)
@@ -34,20 +43,44 @@ class SearchGroupViewModelTest {
     @Test
     fun `Given a group name - When call trigger search group - Then return list Group`() {
         /* Given */
-        val testGroup = TestObserver<List<Group>>()
         val groups = mutableListOf<Group>()
-        val groupName = "groupName"
-        val invite = Invite("from", "to", "groupName", true)
-        `when`(groupRepository.getCurrentRequestOfUser(TestUtil.any())).thenReturn(Observable.just(invite))
+        val updateListViewStatus1 = TestObserver<DiffUtil.DiffResult>()
+        groups.add(Group("id", "name", "token", "ownerId", "", ""))
+        groups.add(Group("id", "name", "token", "ownerId", "", ""))
+
         `when`(groupRepository.searchGroup(TestUtil.any())).thenReturn(Observable.just(groups))
 
-
         /* When */
-        groupRepository.searchGroup(groupName).subscribe(testGroup)
-        viewModel.triggerSearchGroup().subscribe(testGroup)
+        viewModel.updateAutocompleteList
+                .subscribe(updateListViewStatus1)
+        viewModel.eventAfterTextChanged("groupName")
 
         /* Then */
-        testGroup.assertValue { it == groups }
+        updateListViewStatus1.assertValue {
+            it.dispatchUpdatesTo(object : ListUpdateCallback {
+                override fun onChanged(position: Int, count: Int, payload: Any?) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onMoved(fromPosition: Int, toPosition: Int) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onInserted(position: Int, count: Int) {
+                    Assert.assertThat(position, CoreMatchers.`is`(0))
+                    Assert.assertThat(count, CoreMatchers.`is`(2))
+                }
+
+                override fun onRemoved(position: Int, count: Int) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+            })
+            true
+        }
+
+        Assert.assertThat(viewModel.groups.size, CoreMatchers.`is`(2))
+        Assert.assertThat(viewModel.groups[0].id, CoreMatchers.`is`("id"))
+        Assert.assertThat(viewModel.groups[0].token, CoreMatchers.`is`("token"))
     }
 
     @Test
