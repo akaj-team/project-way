dec368cccc98c77c70f0c1d94d77658deaeddc40
at-datbui <dat.bui@asiantech.vn>
2017-12-22 10:45:49 +0700

    change api test

--- app/src/test/java/vn/asiantech/way/api/GoogleApiTest.kt
+++ app/src/test/java/vn/asiantech/way/api/GoogleApiTest.kt
@@ -2,13 +2,12 @@ package vn.asiantech.way.api
 
 import io.reactivex.observers.TestObserver
 import org.hamcrest.CoreMatchers.*
+import org.junit.Assert
 import org.junit.Assert.assertThat
 import org.junit.FixMethodOrder
 import org.junit.Test
 import org.junit.runners.MethodSorters
-import vn.asiantech.way.data.model.AutoCompleteResult
-import vn.asiantech.way.data.model.LocationAddress
-import vn.asiantech.way.data.model.ResultPlaceDetail
+import vn.asiantech.way.data.model.*
 import vn.asiantech.way.data.source.remote.googleapi.ApiService
 import vn.asiantech.way.data.source.remote.response.Response
 import vn.asiantech.way.extension.addResponseBody
@@ -105,6 +104,67 @@ class GoogleApiTest {
             assertThat(item.placeId, `is`("ChIJ8bYoOR8YQjERMiVjLDsw3Kg"))
 
             true
+        }
+    }
+
+    @Test
+    fun `Given mock two lat lng point - When request google distance matrix api - Then return resultDistance object`() {
+        /* Given */
+        val test = TestObserver<ResultDistance>()
+        val origin = "16.083833,108.243501"
+        val destination = "16.084153,108.242214"
+        ApiSuiteTest.server.addResponseBody("locationDistance.json")
+
+        /* When */
+        restClient.getLocationDistance(origin, destination).subscribe(test)
+
+        /* Then */
+        val request = ApiSuiteTest.server.takeRequest()
+        Assert.assertThat(request.method.toUpperCase(), `is`("GET"))
+        Assert.assertThat(request.requestUrl.queryParameterNames(), hasItems("units", "origins", "destinations"))
+        Assert.assertThat(request.requestUrl.queryParameter("origins"), `is`("16.083833,108.243501"))
+        Assert.assertThat(request.requestUrl.queryParameter("destinations"), `is`("16.084153,108.242214"))
+
+        test.assertValue {
+            Assert.assertThat(it.rows.size, `is`(1))
+            val item = it.rows[0].elements[0]
+            Assert.assertThat(item.distance.text, `is`("0.4 km"))
+            Assert.assertThat(item.distance.value, `is`(351))
+            Assert.assertThat(item.duration.text, `is`("1 min"))
+            Assert.assertThat(item.duration.value, `is`(76))
+
+            it.rows.isNotEmpty()
+        }
+    }
+
+    @Test
+    fun `Given mock an url - When request google road api - Then return resultRoad object`() {
+        /* Given */
+        val test = TestObserver<ResultRoad>()
+        val url = "roads.googleapis.com/v1/snapToRoads?path=16.0798071,108.2364393|16.0803531,108.2354526&interpolate=true&key=AIzaSyCZc4PAEpeVC18QnS5fPBt5hk3EFMbFjj8"
+        ApiSuiteTest.server.addResponseBody("googleSnapToRoad.json")
+
+        /* When */
+        restClient.getListLocation(url).subscribe(test)
+
+        /* Then */
+        val request = ApiSuiteTest.server.takeRequest()
+        Assert.assertThat(request.method.toUpperCase(), `is`("GET"))
+        Assert.assertThat(request.requestUrl.queryParameterNames(), hasItems("path", "interpolate", "key"))
+        Assert.assertThat(request.requestUrl.queryParameter("path"), `is`("16.0798071,108.2364393|16.0803531,108.2354526"))
+        Assert.assertThat(request.requestUrl.queryParameter("interpolate"), `is`("true"))
+        Assert.assertThat(request.requestUrl.queryParameter("key"), `is`("AIzaSyCZc4PAEpeVC18QnS5fPBt5hk3EFMbFjj8"))
+
+        test.assertValue {
+            Assert.assertThat(it.locationRoads.size, `is`(9))
+            Assert.assertThat(it.locationRoads[0].point.latitude, `is`(16.079881331167627))
+            Assert.assertThat(it.locationRoads[0].point.longitude, `is`(108.23634932975393))
+            Assert.assertThat(it.locationRoads[0].placeId, `is`("ChIJVXmLIycYQjERpY4bM9aTUG8"))
+            Assert.assertThat(it.locationRoads[8].point.latitude, `is`(16.080934072276641))
+            Assert.assertThat(it.locationRoads[8].point.longitude, `is`(108.23596334365941))
+            Assert.assertThat(it.locationRoads[8].placeId, `is`("ChIJnXA9wiAYQjERQ1CTfr7tnSE"))
+
+            it.locationRoads.isNotEmpty()
         }
     }
 }
