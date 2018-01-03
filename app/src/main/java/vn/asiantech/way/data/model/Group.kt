35dab2815609b586a36ebbf2b90f3a2a4ff51343
Hao Le K <hao.le2@asiantech.vn>
2018-01-03 09:54:33 +0700

    fix bot's comments

--- app/src/main/java/vn/asiantech/way/data/model/Group.kt
+++ app/src/main/java/vn/asiantech/way/data/model/Group.kt
@@ -1,15 +1,10 @@
 package vn.asiantech.way.data.model
 
-import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 /**
  * Copyright © 2017 Asian Tech Co., Ltd.
  * Created by cuongcaov on 09/11/2017
  */
-data class Group(val id: String,
-                 var name: String,
-                 var token: String,
-                 var ownerId: String,
-                 val created_at: String,
-                 var modified_at: String) : Serializable
+data class Group(val id: String, var name: String, var token: String,
+                 var ownerId: String, val created_at: String, var modified_at: String) : Serializable
